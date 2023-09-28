437e94942473a829862a6e30651c6bdc05271fd2
davydenkomax <davydenko.3x3x3@gmail.com>
2023-09-28 15:38:23 +0700

    v2

--- L3/L3.cpp
+++ L3/L3.cpp
@@ -14,7 +14,9 @@ int main(){
     cin >> num1;
     cout << "Введите второе число: ";
     cin >> num2;
-    if ((num1 % 2 != 0) && (num2 % 2 != 0)) {
+
+    // Проверка num1 и num2 на нечетность
+    if ((num1 % 2 != 0) && (num2 % 2 != 0)) { 
         cout << "Истина";
     }
     else {
@@ -28,7 +30,9 @@ int main(){
     cin >> number1;
     cout << "Введите второе число: ";
     cin >> number2;
-    if (number1 >= 0) {
+
+    // Проверка number1 на неотрицательность
+    if (number1 >= 0) { 
         cout << "Ответ: " << number2 * 1000;
     }
     else {
@@ -44,13 +48,16 @@ int main(){
     cout << "Введите значение x: ";
     cin >> x;
 
+    // Проверка значений x
     if (x <= -1) {
         cout << sin(log(abs(x))) << endl;
     }
     else if ((x > -1) && (x < 15)) {
+        // Если x ∈ (-1; 15) 
         cout << pow((4 * x + b), 2) << endl;
     }
     else {
+        // Если x >= 15
         cout << 1 / (pow(a, 2) + pow(x, 2)) << endl;
     }
 
@@ -61,6 +68,7 @@ int main(){
     cout << "Введите оценку (от 0 до 5): ";
     cin >> mark;
 
+    // Провнрка значния переменной mark
     switch (mark) {
     case 5: cout << "Отлично"; break;
     case 4: cout << "Хорошо"; break;
